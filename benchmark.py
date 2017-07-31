d6362c2aae47c0a0af285cad9da4501bb019819d
Kai Fricke <k@ifricke.com>
2017-07-31 14:28:24 +0200

    added some documentation

--- benchmark.py
+++ benchmark.py
@@ -14,7 +14,21 @@
 # ==============================================================================
 
 """
-OpenAI gym execution
+TensorForce benchmarking.
+
+Usage:
+
+```bash
+python benchmark.py [-o output] <algorithm> <gym_id>
+```
+
+`algorithm` specifies which config file to use. You can pass the path to a valid json config file, or a string
+indicating which prepared config to use (e.g. `dqn2015`).
+
+`gym_id` should be a valid [OpenAI gym ID](https://gym.openai.com/envs)
+
+`output` is an optional parameter to set the output (pickle) file. If omitted, output will be saved in `./benchmarks`.
+
 """
 
 from __future__ import absolute_import
